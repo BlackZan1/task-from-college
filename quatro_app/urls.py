45e1b1ac2dad59f77514fa558cac2f7ec4256c7a
Mac <mac@Macs-MacBook.local>
2021-04-18 20:02:18 +0600

    new list page view & html added

--- quatro_app/urls.py
+++ quatro_app/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls import static
 
 # views
 from .views import MainPage, ListPage
@@ -28,3 +30,4 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
