d521791666d19e677d6111be56ea8e6f58023286
Mac <mac@Macs-MacBook.local>
2021-04-18 20:08:34 +0600

    bug fixes

--- quatro_app/urls.py
+++ quatro_app/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
-from django.conf.urls import static
+from django.conf.urls.static import static
 
 # views
 from .views import MainPage, ListPage
